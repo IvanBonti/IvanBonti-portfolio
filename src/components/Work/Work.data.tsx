e4c7a172f8500114a53ce021c68923130a024cd8
Ivan Bonti <ivanbonti300@gmail.com>
2023-10-16 00:31:35 -0300

    object project added

--- src/components/Work/Work.data.tsx
+++ src/components/Work/Work.data.tsx
@@ -2,5 +2,21 @@ import { SiTailwindcss, SiGithub, SiReact, SiNextdotjs, SiTestinglibrary } from 
 
 export const cardContent = [
   {
+    id: 0,
+    title: "Calculator",
+    description:
+      "Calculator project made by React and Tailwind CSS. ",
+    imageUrl: "/work-1.jpeg",
+    skills: [
+      {
+        icon: <SiReact size="40" />
+      },
+      {
+        icon: <SiTailwindcss size="40" />
+      },
+      {
+        icon: <SiTestinglibrary size="40" />
+      },
+    ]
     },
   ];
